5c9e4e01b86b6b1233e7063279ba0d4afc0846d5
Alp Eren Can <alperenc@gmail.com>
2015-08-11 15:07:30 +0300

    Implemented three different ways to navigate between two VCs

---
+++ Roshambo/GameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GameViewController.swift
+//  Roshambo
+//
+//  Created by Alp Eren Can on 29/07/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func randomPlay() -> Int {
+        return Int(1 + arc4random() % 3)
+    }
+    
+
+    @IBAction func playRock(sender: AnyObject) {
+        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+        
+        controller.computer = randomPlay()
+        controller.player = 1
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func playPaper(sender: AnyObject) {
+        performSegueWithIdentifier("playPaper", sender: sender)
+        
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! ResultViewController
+        controller.computer = randomPlay()
+        
+        if segue.identifier == "playPaper" {
+            controller.player = 2
+        } else if segue.identifier == "playScissors" {
+            controller.player = 3
+        }
+
+    }
+}
---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by Alp Eren Can on 29/07/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var player: Int?
+    var computer: Int?
+
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        displayResults()
+    }
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func displayResults() {
+        if player == 1 && computer == 2 {
+            resultLabel.text = "Paper covers rock, you lose!"
+            resultImageView.image = UIImage(named: "PaperCoversRock")
+        } else if player == 1 && computer == 3 {
+            resultLabel.text = "Rock crushes scissors, you win!"
+            resultImageView.image = UIImage(named: "RockCrushesScissors")
+        } else if player == 2 && computer == 1 {
+            resultLabel.text = "Paper covers rock, you win!"
+            resultImageView.image = UIImage(named: "PaperCoversRock")
+        } else if player == 2 && computer == 3 {
+            resultLabel.text = "Scissors cut paper, you loose!"
+            resultImageView.image = UIImage(named: "ScissorsCutPaper")
+        } else if player == 3 && computer == 1 {
+            resultLabel.text = "Rock crushes scissors, you lose!"
+            resultImageView.image = UIImage(named: "RockCrushesScissors")
+        } else if player == 3 && computer == 2 {
+            resultLabel.text = "Scissors cut paper, you win!"
+            resultImageView.image = UIImage(named: "ScissorsCutPaper")
+        } else {
+            resultLabel.text = "It's a tie"
+            resultImageView.image = UIImage(named: "itsATie")
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
